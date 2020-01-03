94956fc3b77c620bb3612c1c7cf05ba7d7233dc7
xXValhallaCoderXx <renate.gouveia@gmail.com>
2020-01-03 19:23:19 +0800

    Branch ready for tutorial

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,36 +1,14 @@
 import React from "react"
-import { graphql } from "gatsby"
 import Layout from "components/layout"
 import SEO from "components/seo"
 
 const IndexPage = ({ data }) => {
-  console.log("data: ", data.allMarkdownRemark.edges[0].node)
-  const blog = data.allMarkdownRemark.edges[0].node;
-  console.log("BLOG: ", blog.html);
   return (
     <Layout>
       <SEO title="Home" />
       <h1>Hi Welcome To The Pokemon Blog!</h1>
-      <div dangerouslySetInnerHTML={{ __html: blog.html }} />
     </Layout>
   )
 }
 
 export default IndexPage
-
-export const QUERY = graphql`
-  {
-    allMarkdownRemark {
-      edges {
-        node {
-          frontmatter {
-            title
-            intro
-          }
-          excerpt
-          html
-        }
-      }
-    }
-  }
-`
