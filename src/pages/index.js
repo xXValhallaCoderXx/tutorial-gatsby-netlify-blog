57975d62175976f201c3e4115d6a6e65c59d50ab
xXValhallaCoderXx <renate.gouveia@gmail.com>
2020-01-03 19:23:38 +0800

    remove unused code

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import React from "react"
 import Layout from "components/layout"
 import SEO from "components/seo"
 
-const IndexPage = ({ data }) => {
+const IndexPage = () => {
   return (
     <Layout>
       <SEO title="Home" />
